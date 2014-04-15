f7f23537acb5a2e525fab5ccbae38fb1ea3c7086
Amanda McDermott <amandapmcdermott@gmail.com>
2014-04-14 21:05:23 -0400

    Fixed the audio objects & check comparison
    The audio stuff wouldn’t play— needed to replace the entire audio tag,
    not just the source refs. Also made changes to getRandomInterval
    selection & comparison.
    
    Need to link the js back with the css for a “correct” class and events
    that occur henceforth.

--- js/index.js
+++ js/index.js
@@ -8,19 +8,19 @@
 
 // master array contains all the possible options
 var masterArray = [
-	{steps:0, name:"unison"},
-	{steps:1, name:"min2"},
-	{steps:2, name:"maj2"},
-	{steps:3, name:"min3"},
-	{steps:4, name:"maj3"},
-	{steps:5, name:"perf4"},
-	{steps:6, name:"tritone"},
-	{steps:7, name:"perf5"},
-	{steps:8, name:"min6"},
-	{steps:9, name:"maj6"},
-	{steps:10, name:"min7"},
-	{steps:11, name:"maj7"},
-	{steps:12, name:"octave"}
+	{steps:0, named:"unison"},
+	{steps:1, named:"min2"},
+	{steps:2, named:"maj2"},
+	{steps:3, named:"min3"},
+	{steps:4, named:"maj3"},
+	{steps:5, named:"perf4"},
+	{steps:6, named:"tritone"},
+	{steps:7, named:"perf5"},
+	{steps:8, named:"min6"},
+	{steps:9, named:"maj6"},
+	{steps:10, named:"min7"},
+	{steps:11, named:"maj7"},
+	{steps:12, named:"octave"}
 ],mutableMasterArray=[],dynamicArray=[];
 
 // builds an dynamic array based on the IDs of buttons w/ the class "enabled"
@@ -35,39 +35,44 @@ var masterArray = [
 	}
 
 	Array.prototype.indexOfIdInObject=function(id){
-	var res=-1;
-	this.forEach(function(el,i){
-		if(el.name==id){
-			res=i;
-		}
-	});
-	return res;
-};
+		var res=-1;
+		this.forEach(function(el,i){
+			if(el.named==id || el.steps==id){
+				res=i;
+			}
+		});
+		return res;
+	};
 
 // 52 is the max, 40 is the min --> these correspond to the audio sample, which are named based on their note's position on an 88-key keyboard
-function getRandomSample(){
-	var randNum1 = Math.floor(Math.random()*(52-40+1)+40);
-	$('.sample.first').html('<source src="audio/'+randNum1+'.mp3" type="audio/mpeg"> <source src="audio/'+randNum1+'.ogg" type="audio/ogg"> <source src="audio/'+randNum1+'.wav" type="audio/wav"> Your browser does not support the audio element.');
-	var randNum2 = Math.floor(Math.random()*(52-40+1)+40);
-	$('.sample.second').html('<source src="audio/'+randNum2+'.mp3" type="audio/mpeg"> <source src="audio/'+randNum2+'.ogg" type="audio/ogg"> <source src="audio/'+randNum2+'.wav" type="audio/wav"> Your browser does not support the audio element.');
-}
-
 // play the two samples one after another (how do I do this?)
-function getRandomInterval() {
-	getRandomSample();
+// also needs to compare randNum1 with randNum2 and determine the difference --> "steps" in the array's objects (i.e. 3 steps apart = min3)
+var stepDiff = 0;
+var intName = "";
+function getRandomInterval(){
+	var randNum1 = Math.floor(Math.random()*(dynamicArray.length)+40);
+	var randNum2 = Math.floor(Math.random()*(dynamicArray.length)+40);
+	stepDiff = randNum1 - randNum2;
+	var intName=dynamicArray[Math.abs(dynamicArray.indexOfIdInObject(stepDiff))].named;
+	$("#"+intName).addClass('correct');
+	$('.sample.first').empty().append('<audio controls><source src="audio/'+randNum1+'.mp3" type="audio/mpeg"> <source src="audio/'+randNum1+'.ogg" type="audio/ogg"> <source src="audio/'+randNum1+'.wav" type="audio/wav"> Your browser does not support the audio element.</audio>');
+	$('.sample.second').append('<audio controls><source src="audio/'+randNum2+'.mp3" type="audio/mpeg"> <source src="audio/'+randNum2+'.ogg" type="audio/ogg"> <source src="audio/'+randNum2+'.wav" type="audio/wav"> Your browser does not support the audio element.</audio>');
+//	console.log(randNum1, randNum2);
 }
 
 $(document).ready(function() {
 	$('.gamestart').on('click', function(){
 		$(this).toggleClass('hide');
-		$("audio").toggleClass('hide');
+		$(".sample").toggleClass('hide');
+		buildArray();
 		getRandomInterval();
 	});
 
 	//if the user clicks on a <i> with fa-check-square-o, then the <i> class changes to fa-square-o and the button is given the "disabled" class
-	$('i').on('click', function(){
+	$('i.fa-fw').on('click', function(){
 		$(this).toggleClass('fa-check-square-o').toggleClass('fa-square-o').parent().toggleClass('enabled').toggleClass('disabled');
 		buildArray();
+		getRandomInterval();
 	});
 	// the dynamicArray is rebuilt with only the intervals w/ the "enabled" class
 
